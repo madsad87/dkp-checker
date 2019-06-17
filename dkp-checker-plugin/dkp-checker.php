1c9c4d2a1008244c726fea111d024cb10d50159f
Madison Sadler <madison.sadler87@gmail.com>
2019-06-17 12:13:15 -0500

    adding basic name plate for the plugin

--- dkp-checker-plugin/dkp-checker.php
+++ dkp-checker-plugin/dkp-checker.php
@@ -1,5 +1,10 @@
 <?php
-/**
- * Plugin Name: DKP Checker
- *
- */
+/*
+Plugin Name: DKP Checker
+Plugin URI: https://github.com/madsad87/dkp-checker
+Description: A DKP log for Wow Classic, designed as a plugin for a wordpress site.
+Author: Madison Sadler and Gary Singer
+Version: 1.0
+Author URI: madisonsadler.com
+*/
+
